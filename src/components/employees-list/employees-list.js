6ff563d2ad57d8c6762253e3fc2c94b2333119d2
Anderey Prokhorov <beta737@yandex.ru>
2022-01-28 20:48:25 +0300

    list-item

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -7,9 +7,9 @@ import './employees-list.css';
 const EmployeesList = () => {
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            <EmployeesListItem name="Andrey P." salary={5000}/>
+            <EmployeesListItem name="John M." salary={2000}/>
+            <EmployeesListItem name="Anna R." salary={2700}/>
         </ul>
     )
     
